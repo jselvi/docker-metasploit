59fc53052bf6639aa79542e949f106bc30b87331
Jose Selvi <jselvi@pentester.es>
2017-11-22 19:16:29 +0100

    Fix problem with updates: user.name and user.email

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source /usr/local/rvm/scripts/rvm
+/usr/bin/git config --global user.name "msfuser"
+/usr/bin/git config --global user.email "user@msf.int"
 /opt/msf/msfupdate --git-branch master
